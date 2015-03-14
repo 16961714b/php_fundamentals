bc41d837b4f83f7fd1c30ba2800150750742b73c
16961714b <16961714b@users.noreply.github.com>
2015-03-14 10:59:36 +0100

    Test index.php 001

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	echo 'Hello world!';
+	//you can also use 'print' instead of 'echo' command
+	//this is a single line comment
+	/*this is a commnet block
+	it can span across
+	multiple lines
+	*/
+?>
