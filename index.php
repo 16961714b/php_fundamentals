645bdaeedabc50a7ef4dc3d4f7ce80d8f6f57ff4
16961714b <16961714b@users.noreply.github.com>
2015-03-14 17:32:34 +0100

    Changes to be committed: 	deleted:    ../../../../index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo 'Hello world!';
-	//you can also use 'print' instead of 'echo' command
-	//this is a single line comment
-	/*this is a commnet block
-	it can span across
-	multiple lines
-	*/
-?>
