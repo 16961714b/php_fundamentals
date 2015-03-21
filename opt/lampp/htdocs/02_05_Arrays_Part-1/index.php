efa0d900d4f57aed2b29078102afd27641e08cd1
16961714b <16961714b@users.noreply.github.com>
2015-03-21 18:00:07 +0100

    new file:   index.php 	new file:   index.php.bak0 	new file:   index.php.bak1 	new file:   index.php.bak2 	new file:   index.php.bak3 	new file:   index.php.bak4 	new file:   index.php.bak5 	new file:   index.php.bak6 	new file:   index.php.bak7 	new file:   index.php.bak8 	modified:   ../02_04_Displaying-Errors/index.php

---
+++ opt/lampp/htdocs/02_05_Arrays_Part-1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$months = array('january', 'february', 'march', 'april', 'may');
+
+var_dump($months)
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arrays</title>
+</head>
+<body>
+	<h1>Arrays</h1>
+</body>
+</html>
